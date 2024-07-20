1e408d414b214ac782b37d10adbb26bdc5269037
ivanpachecos <ivanpachecos@yahoo.com>
2024-07-20 13:24:21 -0500

    loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
   for (const idx of array) {
     const value = array[idx];
-    // eslint-disable-next-line no-param-reassign
-    array[idx] = appendString + value;
+    result.push(appendString + value);
   }
 
   return array;
